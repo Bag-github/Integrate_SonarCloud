f4f93b794cdb82207887a276d213217ad9e7c28b
Muhammad Arsalan Khan <59295340+Bag-github@users.noreply.github.com>
2022-05-23 12:14:21 +0500

    some text changes

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace azure_devops_SonarCloud
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test application!");
         }
     }
 }
